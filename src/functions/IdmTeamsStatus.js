031f858e655108cd14a3c47fc3a88210025a18ef
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-05-09 14:50:17 +0200

    fix: added teams status timer trigger

---
+++ src/functions/IdmTeamsStatus.js
@@ -0,0 +1,19 @@
+import { app } from '@azure/functions';
+import { teamsStatusAlert } from '../lib/fint-organization-fixed/teams-status-alert.js';
+import { logger } from '@vestfoldfylke/loglady';
+
+app.timer('IdmTeamsStatus', {
+    schedule: '0 0 7,18 * * *', // Every day at 07:00 and 18:00
+    handler: async () => {
+        logger.logConfig({
+            prefix: 'azf-fint-folk - IDM Teams Status'
+        });
+        logger.info('Running timer trigger');
+
+        try {
+            await teamsStatusAlert()
+        } catch (error) {
+            logger.errorException(error, 'Teams status alert failed... {err}', error.stack || error.toString());
+        }
+    }
+});
